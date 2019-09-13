6e245d2f419cf74e3f8fb5c4c47d3a6eb66c454f
GoldMain <3559252671@qq.com>
2019-09-13 23:52:42 +0800

    null

--- main/java/ink/mhxk/mooncoke/client/ClientProxy.java
+++ main/java/ink/mhxk/mooncoke/client/ClientProxy.java
@@ -1,8 +1,24 @@
 package ink.mhxk.mooncoke.client;
 
 import ink.mhxk.mooncoke.common.CommonProxy;
+import net.minecraft.block.Block;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class ClientProxy
 extends CommonProxy {
+    @SubscribeEvent
+    public void registerModels(ModelRegistryEvent event){
 
+    }
+    public void registerBlockModel(Block block){
+        registerItemModel(new ItemBlock(block).setRegistryName(block.getRegistryName()));
+    }
+    public void registerItemModel(Item item){
+        ModelLoader.setCustomModelResourceLocation(item,0,new ModelResourceLocation(item.getRegistryName(),"inventory"));
+    }
 }
--- main/java/ink/mhxk/mooncoke/common/CommonProxy.java
+++ main/java/ink/mhxk/mooncoke/common/CommonProxy.java
@@ -1,4 +1,15 @@
 package ink.mhxk.mooncoke.common;
 
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraftforge.event.RegistryEvent;
+
 public class CommonProxy {
+
+    public void registerItems(RegistryEvent.Register<Item> event){
+
+    }
+    public void registerBlocks(RegistryEvent.Register<Block> event){
+
+    }
 }
