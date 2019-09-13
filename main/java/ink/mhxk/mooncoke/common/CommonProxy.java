9c95ede92340fa5b1aad49c36de1946b18406535
GoldMain <3559252671@qq.com>
2019-09-13 23:53:48 +0800

    null

--- main/java/ink/mhxk/mooncoke/common/CommonProxy.java
+++ main/java/ink/mhxk/mooncoke/common/CommonProxy.java
@@ -3,12 +3,14 @@ package ink.mhxk.mooncoke.common;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class CommonProxy {
-
+    @SubscribeEvent
     public void registerItems(RegistryEvent.Register<Item> event){
 
     }
+    @SubscribeEvent
     public void registerBlocks(RegistryEvent.Register<Block> event){
 
     }
